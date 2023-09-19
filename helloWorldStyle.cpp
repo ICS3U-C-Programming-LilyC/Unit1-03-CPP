0f09a3a886bbb4f519e74a941e451197888fc36c
Lily Carroll <lily.carroll@stu.ocsb.ca>
2023-09-19 01:05:46 +0000

    Fixed errors using cpplint.

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2023 Lily Carroll All rights reserved.
+// .
+// Created by: Lily Carroll
+// Date: Sept/18/2023
+// This program displays "Hello, World with style".
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World with style!" << std::endl;
+    std::cout << "C++ is a more complex language compared to python."
+              << std::endl;
+}
